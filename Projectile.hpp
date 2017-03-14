4e4d5e89d17900087e0f18241f40f988d236980c
Elisabeth Eriksson <elly.eriksson91@gmail.com>
2017-03-14 10:45:43 +0100

    Forgot to push this

--- Projectile.hpp
+++ Projectile.hpp
@@ -1,1 +1,20 @@
-// Projectiles
+// Projectile.hpp
+#ifndef projectile_hpp
+#define projectile_hpp
+
+#include "Actor.hpp"
+
+class Projectile : public Actor {
+	public:
+	Projectile();
+	Projectile(int owner, int damage);
+	~Projectile();
+	void act(Actor &a);
+	
+	private:
+	int owner;
+	int damage;
+
+};
+
+#endif
